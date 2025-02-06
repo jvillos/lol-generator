8d6fc6590ae7effb0a6177b4c84b764a67ff6150
jvillos <jvillosl14@gmail.com>
2025-02-06 13:29:01 +0100

    final sin pikachu

--- src/components/RandomGif.tsx
+++ src/components/RandomGif.tsx
@@ -1,9 +1,20 @@
 import '../App.css'
 
+import gif0 from '../assets/lol0.gif';
+import gif1 from '../assets/lol1.gif';
+import gif2 from '../assets/lol2.gif';
+import gif3 from '../assets/lol3.gif';
+import gif4 from '../assets/lol4.gif';
+import gif5 from '../assets/lol5.gif';
+import gif6 from '../assets/lol6.gif';
+
 function RandomGif() {
   const num:number = Math.round(Math.random() * 6);
+
+  const gifs = [gif0, gif1, gif2, gif3, gif4, gif5, gif6];
+
   return (
-    <img src={`../assets/lol${num}.gif`} alt="LOL GIF"/>
+    <img src={gifs[num]} alt="LOL GIF"/>
   )
 }
 
